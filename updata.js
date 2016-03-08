7fe69afe49e4664e323b314fe025d4edd43d69f3
Vincent Sisk <vinnysisk@gmail.com>
2016-03-07 21:49:25 -0700

    add some basic error handling for hg calls

--- updata.js
+++ updata.js
@@ -5,36 +5,65 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 if (!fs.existsSync('.hgsub')) {
-    console.error(chalk.red.bold('no .hgsub file found in directory'));
+    console.error(errorMessage('no .hgsub file found in directory'));
     exit(1);
 }
-    
+
 const options = require('./options');
-    
+
 let hgRoot = pwd();
 
 cat('.hgsub')
     .split('\r\n')
     .filter(line => line.length > 0)
     .map(line => line.split('=')[0].trim())
-    .map(path => options.pullOnly ? pull(path) : pullAndUpdate(path, options.tag));
+    .map(path => options.pullOnly ? pull(path) : update(path, options.tag));
 
-function pull(path) {
-    cd(path);
-    
-    echo(chalk.magenta(`pulling changes for ${chalk.green(path)}`));
-    exec('hg pull --verbose');
+function pull(repo) {
+    cd(repo);
+
+    echo(chalk.magenta(`pulling changes for ${repoMessage(repo)}`));
+    let exitCode = exec('hg pull --verbose').code;
     
     cd(hgRoot);
+    
+    if (exitCode !== 0) {
+        console.error(errorMessage(`pull for ${repoMessage(repo)} failed`));
+        return false;
+    }
+    
+    return true;
 }
 
-function pullAndUpdate(path, tag) {
-    pull(path);
-    
-    cd(path);
+function update(repo, tag) {
+    if (!pull(repo)) {            
+        return false;
+    }
 
-    echo(chalk.magenta(`updating ${chalk.green(path)} to tag: ${chalk.yellow(tag)}`));
-    exec(`hg update ${tag}`);
+    cd(repo);
+
+    echo(chalk.magenta(`updating ${repoMessage(repo)} to tag: ${tagMessage(tag)}`));
+    
+    let exitCode = exec(`hg update ${tag}`).code;
     
     cd(hgRoot);
+    
+    if (exitCode !== 0) {
+        console.error(errorMessage(`update for ${repoMessage(repo)} failed`));
+        return false;
+    }
+    
+    return true;
+}
+
+function errorMessage(message) {
+    return chalk.red.bold(message);
+}
+
+function repoMessage(repo) {
+    return chalk.green(repo);
+}
+
+function tagMessage(tag) {
+    return chalk.yellow(tag);
 }
