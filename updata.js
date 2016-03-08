28e4e36f6ef073ef02e3a2b49c39d8ea549fa926
Vincent Sisk <vinnysisk@gmail.com>
2016-03-07 20:28:18 -0700

    add hg update functionality
    refactor to create a functional pipeline

--- updata.js
+++ updata.js
@@ -3,40 +3,47 @@
 require('shelljs/global');
 const fs = require('fs');
 
+if (!fs.existsSync('.hgsub')) {
+    console.error('no .hgsub file found in directory');
+    exit(1);
+}
+
 //import { Command } from 'commander';
 // let updata = new Command();
 let updata = require('commander');
 
 updata
     .version('0.1.0')
+    .option('-t, --tag <id>', 'tag to update to [defaults to tip]')
+    .option('-p, --pull-only', 'only pull latest changes from remote')
     .parse(process.argv);
+
+updata.tag = updata.tag || 'tip';
     
 let hgRoot = pwd();
 
-for (let path in getSubRepoPaths()) {
+cat('.hgsub')
+    .split('\r\n')
+    .filter(line => line.length > 0)
+    .map(line => line.split('=')[0].trim())
+    .map(path => updata.pullOnly ? pull(path) : pullAndUpdate(path, updata.tag));
+
+function pull(path) {
     cd(path);
     
     echo(`pulling changes for ${path}`);
-    pullChanges();
-    
-    cd(root);
-}
-
-function getSubRepoPaths() {
-    if (fs.existsSync('.hgsub')) {
-        let hgsub = cat('.hgsub');
-        let paths = hgsub.split('\r\n')
-            .filter(line => line.length > 0)
-            .map(line => line.split('=')[0].trim()
-            );
-        //echo(paths);
-    } else {
-        console.error('no .hgsub file found in directory');
-        // might want to throw instead of exit within this function
-        exit(1);
-    }
-}
-
-function pullChanges() {
     exec('hg pull --verbose');
+    
+    cd(hgRoot);
+}
+
+function pullAndUpdate(path, tag) {
+    pull(path);
+    
+    cd(path);
+
+    echo(`updating ${path} to tag: ${tag}`);
+    exec(`hg update ${tag}`);
+    
+    cd(hgRoot);
 }
